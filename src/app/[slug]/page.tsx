61aff65d0ef296d40559dd841c81d0698e8c66da
Tomas Vazquez <vazquezt2018@gmail.com>
2023-02-22 15:50:39 -0300

    added: tailwind css

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -2,6 +2,7 @@ import api from "@/api";
 import Link from "next/link";
 import React from "react";
 import {notFound} from 'next/navigation'
+import '../../styles.css'
 
 type Props = {
   params: {
@@ -20,10 +21,10 @@ export default async function Slug({params: {slug}}: Props)   {
   const links = await api.links.fetch(user.url);
 
   return (
-    <main>
-      <h1>Usuarios</h1>
-      <ul>
-      {links.map((link) => <li key = {link.url}>
+    <main className="container flex flex-col justify-center items-center text-center">
+      <h1 className="text-3xl font-bold underline">Links de {slug}</h1>
+      <ul className = "flex flex-col gap-2 mt-3 w-64">
+      {links.map((link) => <li className = "bg-sky-500/50 p-1 rounded-lg capitalize"key = {link.url}>
         <Link href = {link.url}>{link.label}</Link>
       </li>)}
       </ul>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,16 @@
 import api from "@/api";
 import Link from "next/link";
 import React from "react";
+import '../styles.css';
 
 export default async function Home() {
   const users = await api.user.list();
 
   return (
-    <main>
-      <h1>Tomas</h1>
-      <ul>
-      {users.map((user) => <li key = {user.url}>
+    <main className = "container flex flex-col justify-center items-center">
+      <h1 className="text-3xl font-bold underline">Lista de usuarios:</h1>
+      <ul className = ' flex gap-2 flex-col mt-3'>
+      {users.map((user) => <li className = 'bg-sky-500/50 p-1 rounded-lg text-center' key = {user.url}>
         <Link href = {`/${user.slug}`}>{user.slug}</Link>
       </li>)}
       </ul>
